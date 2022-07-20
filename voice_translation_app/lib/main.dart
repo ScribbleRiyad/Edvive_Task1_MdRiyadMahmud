6537b2d4b18217bd7b71a60e62fe78d9c41a4499
scribble-riyad <scribbleriyad@gmail.com>
2022-07-20 20:02:39 +0600

    first commit

---
+++ voice_translation_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:voice_translation_app/View/Ui/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Voice Trasnlation App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
